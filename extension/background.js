caeddf32df2a9e6960981ca0925dabdd54b41109
jseakle <jseakle@gmail.com>
2014-08-03 19:39:59 -0700

    no more prefixes, only regexes

--- extension/background.js
+++ extension/background.js
@@ -4,14 +4,24 @@
 	chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
 		var mappingKey = 'nl.sjmulder.urlrewrite.mappings';
 		var mappings = JSON.parse(localStorage[mappingKey] || '[]');
-
+		if (/#NOLOOP$/.exec(details.url)) {
+			return;
+		}
 		for (var i = 0; i < mappings.length; i++) {
 			var mapping = mappings[i];
-			if (details.url.indexOf(mapping.sourceUrl) == 0) {
-				var newUrl = mapping.destinationUrl + details.url.slice(mapping.sourceUrl.length);
-				// console.log('rewriting', details.url, 'to', newUrl);
-				
-				chrome.tabs.update(details.tabId, { url: newUrl })
+			var source_regex = new RegExp(mappings[i].sourceUrl);
+			var match = source_regex.exec(details.url);
+			if (match) {
+				//console.log("MATCHED " + mappings[i].sourceUrl + "\n" + match);
+				var expandedDestination = mapping.destinationUrl;
+				for( i = 1; i < match.length; i++) {
+					expandedDestination = expandedDestination.replace("*"+i, match[i]);
+					//console.log("REPLACING *" +i +" with " + match[i] );
+				}
+				var newUrl = expandedDestination;
+				//console.log('rewriting', details.url, 'to', newUrl);
+
+				chrome.tabs.update(details.tabId, { url: newUrl + "#NOLOOP" })
 				break;
 			}
 		}
