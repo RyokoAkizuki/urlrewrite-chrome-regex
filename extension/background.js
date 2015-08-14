9b80f1ea17abc264a617adfb4768f9e0dfb2849b
Yukino Hayakawa <tennencoll@gmail.com>
2015-08-14 09:58:53 +0800

    use webRequest to capture all requests

--- extension/background.js
+++ extension/background.js
@@ -1,30 +1,40 @@
 (function() {
 	'use strict';
 
-	chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
+	chrome.webRequest.onBeforeRequest.addListener(function(details) {
 		var mappingKey = 'nl.sjmulder.urlrewrite.mappings';
 		var mappings = JSON.parse(localStorage[mappingKey] || '[]');
+		/*
 		if (/#NOLOOP$/.exec(details.url)) {
-			return;
+			return {
+                redirectUrl : details.url
+            };
 		}
+		*/
 		for (var i = 0; i < mappings.length; i++) {
 			var mapping = mappings[i];
 			var source_regex = new RegExp(mappings[i].sourceUrl);
 			var match = source_regex.exec(details.url);
 			if (match) {
-				//console.log("MATCHED " + mappings[i].sourceUrl + "\n" + match);
+				// console.log("MATCHED " + mappings[i].sourceUrl + "\n" + match);
 				var expandedDestination = mapping.destinationUrl;
 				for( i = 1; i < match.length; i++) {
-					expandedDestination = expandedDestination.replace("*"+i, match[i]);
-					//console.log("REPLACING *" +i +" with " + match[i] );
+					expandedDestination = expandedDestination.replace("$"+i, match[i]);
+					// console.log("REPLACING $" +i +" with " + match[i] );
 				}
 				var newUrl = expandedDestination;
-				//console.log('rewriting', details.url, 'to', newUrl);
+				// console.log('rewriting', details.url, 'to', newUrl);
 
-				chrome.tabs.update(details.tabId, { url: newUrl + "#NOLOOP" })
-				break;
+				return {
+	                redirectUrl : newUrl// + "#NOLOOP"
+	            };
 			}
 		}
-	});
+	},
+    {
+        urls : ["<all_urls>"]
+    }, 
+    ["blocking"]
+    );
 
 })();
